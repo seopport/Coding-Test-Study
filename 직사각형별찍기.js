88f77f6425fda347d65d29f4a8e41692435ce020
Jiwon <tj1tj2tj3@naver.com>
2024-01-17 10:09:52 +0900

    0117

--- 직사각형별찍기.js
+++ 직사각형별찍기.js
@@ -1,13 +1,19 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+
+  // solution 함수 호출
+  solution(a, b);
+});
+
 function solution(n, m) {
-  let column = [];
   for (let i = 0; i < m; i++) {
     let row = [];
     for (let j = 0; j < n; j++) {
       row.push("*");
     }
-    column.push(row.join(""));
+    console.log(row.join(""));
   }
-  console.log(column.join("/"));
 }
-
-solution(5, 3);
