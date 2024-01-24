098033c6417cb1592fab57444c74c54ae5d8cbbf
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-01-24 10:12:44 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 3.37 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,15 @@
+function solution(t, p) {
+    var answer = 0;
+    // i번째 부터 i+2번째까지 담고
+    // 그게 p보다 작으면 count + 1
+    for(let i = 0; i<=t.length-p.length; i++){
+        tempT = t.substr(i, p.length);
+        console.log(tempT, tempT*1);
+        if(tempT*1 <= p){
+            answer += 1;
+        }
+    }
+    
+    
+    return answer;
+}
