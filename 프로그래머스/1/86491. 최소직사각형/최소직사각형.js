0a58e5dbc7ad3fb0fb2f57667ad5764244fd9026
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-01-25 10:05:53 +0900

    [level 1] Title: 최소직사각형, Time: 4.46 ms, Memory: 39.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,16 @@
+//모든 명함을 가로로 돌리고 가장 큰 가로값과 가장 큰 세로값
+function solution(sizes) {
+    var answer = 0;
+    let maxWidth = [];
+    let maxHeight = [];
+    
+    for(let i = 0; i<sizes.length; i++){
+        maxWidth.push(sizes[i][0] >= sizes[i][1] ? sizes[i][0] : sizes[i][1])
+        maxWidth.push(Math.max(sizes[i][0], sizes[i][1]))
+        maxHeight.push(Math.min(sizes[i][0], sizes[i][1]))
+    }
+    maxWidth = Math.max(...maxWidth)
+    maxHeight = Math.max(...maxHeight)
+    
+    return maxWidth * maxHeight;
+}
