c85452b523e21fd0a4794790b936fe85b9cc6042
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-07-24 19:09:12 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,14 +1,15 @@
 function solution(price) {
     let sale = 0;
-    
-    // 10만 이상 5퍼
-    // 30만 이상 10퍼
-    // 50만 이상 20퍼
-    if(price >=100000 && price < 300000){
+
+    if(price >=100000){
         sale = 0.05;
-    }else if(price >= 300000 && price < 500000){
+    }
+    
+    if(price >= 300000){
         sale = 0.1;
-    }else if(price >= 500000){
+    }
+    
+    if(price >= 500000){
         sale = 0.2;
     }
     
