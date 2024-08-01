3024777110fa9f5f8e7f5e194cd8ac9db481fe27
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-08-01 18:34:35 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,10 +1,6 @@
 function solution(my_string, num1, num2) {
-    let stringArray = [...my_string];
-    const str1 = my_string[num1];
-    const str2 = my_string[num2];
-    
-    stringArray.splice(num1, 1, str2);
-    stringArray.splice(num2, 1, str1);
+    let stringArray = my_string.split('');
+    [stringArray[num1], stringArray[num2]] = [stringArray[num2], stringArray[num1]]
     
     return stringArray.join('');
 }
