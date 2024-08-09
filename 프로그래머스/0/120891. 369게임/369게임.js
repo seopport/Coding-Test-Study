b6c5e713e7df266bbd332083e03b3fcc3b412834
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-08-09 21:06:03 +0900

    [level 0] Title: 369게임, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.js
+++ 프로그래머스/0/120891. 369게임/369게임.js
@@ -1,8 +1,6 @@
 function solution(order) {
     let answer = 0;
     let count = 0;
-    order.toString().split('').filter((item) => {
-        item === "3" || item === "6" || item === "9" ? count++ : 0;
-    })
-      return count;
+    return order.toString().split('').filter((item) => item === "3" || item === "6" || item === "9").length
+       
 }
