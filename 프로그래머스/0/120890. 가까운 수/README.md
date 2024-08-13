7a90d53c8c68652efa1952f068dda24f9f15f5c3
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-08-13 20:08:14 +0900

    [level 0] Title: 가까운 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 13일 20:06:59
+2024년 08월 13일 20:08:03
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -5,6 +5,6 @@ function solution(array, n) {
     
     const front = array[array.indexOf(n) - 1];
     const back = array[array.indexOf(n) + 1];
-    if (n - front === (n - back) * (-1)) return front;
-    return n - front < (n - back) * (-1) ? front : back;
+    if (n - front === Math.abs(n - back)) return front;
+    return n - front < Math.abs(n - back) ? front : back;
 }
