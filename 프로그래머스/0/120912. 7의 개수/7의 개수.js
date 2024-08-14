d14f6b0197dfcebba200d9c258eea23817c6c019
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-08-14 22:44:09 +0900

    [level 0] Title: 7의 개수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,5 +1,4 @@
 function solution(array) {
-    var answer = 0;
     const charArray = array.reduce((acc, cur) => acc + cur+'', 0);
-    return ~~charArray.match(new RegExp(7, 'g'))?.length
+    return ~~charArray.match(/7/g)?.length
 }
