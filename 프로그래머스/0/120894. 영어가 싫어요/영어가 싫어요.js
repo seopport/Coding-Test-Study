3e9227989f68dda3de572cd3ff0ea3634d4edca4
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-08-14 23:43:02 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,14 @@
+function solution(numbers) {
+    numbers = numbers.replaceAll('one', '1');
+    numbers = numbers.replaceAll('two', '2');
+    numbers = numbers.replaceAll('three', '3');
+    numbers = numbers.replaceAll('four', '4');
+    numbers = numbers.replaceAll('five', '5');
+    numbers = numbers.replaceAll('six', '6');
+    numbers = numbers.replaceAll('seven', '7');
+    numbers = numbers.replaceAll('eight', '8');
+    numbers = numbers.replaceAll('nine', '9');
+    numbers = numbers.replaceAll('zero', '0');
+    
+    return Number(numbers);
+}
