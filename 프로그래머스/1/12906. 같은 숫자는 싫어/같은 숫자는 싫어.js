4e21e0a411506503f7c297e37e0e18f2674d764e
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-10-26 16:02:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.10 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -4,7 +4,7 @@ function solution(arr)
     
     
     for(let i = 0; i < arr.length; i++){
-        if(!answer.includes(arr[i]) || arr[i] !== arr[i-1]){
+        if(arr[i] !== arr[i-1]){
             answer.push(arr[i])
         }
     }
