e469a0b74f624de8bc6abce89c25983c462b5a90
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-10-27 01:23:31 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.87 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -9,6 +9,9 @@ function solution(s) {
             answer[i] = -1;
         }
         answer[0] = -1;
+        
+        // console.log("타겟 문자 :",s[i], "현재 문자를 제외하고 마지막으로 나타난 같은 문자의 인덱스 :", 
+        //             lastIndex, "현재 인덱스 (i) :", i);
     }
     
     return answer;
