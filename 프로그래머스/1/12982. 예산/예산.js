542f40d3fe5aff8155992758f030dc01fa7b2c9f
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-10-26 19:20:52 +0900

    [level 1] Title: 예산, Time: 18.65 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,19 @@
+function solution(d, budget) {
+    let sum = 0;
+    let count = 0;
+    
+    if(d.reduce((acc, cur) => acc + cur) <= budget){
+        return d.length;
+    }else {
+        const sortedD = d.sort((a, b) => a - b);
+        console.log(sortedD)
+        for(let i = 0; i < sortedD.length; i++){
+            sum += sortedD[i];
+            count++;
+            if(sum > budget) {
+                return count - 1;
+            }else if(sum === budget) return count;
+        }
+        }
+        
+}
