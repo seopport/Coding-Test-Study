dc48ce496423e20f50cb49cee9f10802d61676fb
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-10-26 21:36:57 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.58 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644#) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.73 ms
+메모리: 33.7 MB, 시간: 0.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 26일 21:32:40
+2024년 10월 26일 21:36:55
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,14 +1,10 @@
 function solution(numbers) {
-    const sumSet = new Set(); // Set을 사용해 중복 제거
-
-    // 두 개의 수를 더하는 모든 조합 생성
-    for (let i = 0; i < numbers.length; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            sumSet.add(numbers[i] + numbers[j]); // 합을 Set에 추가
+    let sumList = [];
+    const sorted = [...numbers].sort((a, b) => a - b);
+    for(let i = 0; i < sorted.length; i++){
+        for(let j = i + 1; j < sorted.length; j++){
+            sumList.push(sorted[i] + sorted[j])
         }
     }
-
-    // Set을 배열로 변환하고 정렬
-    const answer = Array.from(sumSet).sort((a, b) => a - b);
-    return answer;
-}
+    return [...new Set(sumList)].sort((a, b) => a - b);
+}        
