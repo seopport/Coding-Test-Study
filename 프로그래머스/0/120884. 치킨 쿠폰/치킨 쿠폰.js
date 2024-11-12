b2a75e7adfd2577079541ef0a5e1bc1eee1e4efc
Jiwon Seo <103973797+seopport@users.noreply.github.com>
2024-11-12 17:37:46 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,19 +1,14 @@
 function solution(chicken) {
     var answer = 0;
     let left = 0;
-    while(chicken >= 10){
-        answer += (Math.floor(chicken / 10));
-        left += (chicken % 10);
-        
-        chicken = Math.floor(chicken /= 10);
-        console.log(chicken, left)
-        
+    
+    while(chicken > 1){
+        answer += Math.floor(chicken / 10);
+        left += chicken % 10;
+        chicken = chicken / 10;
     }
     
-    if(left+1 >= 10) left += (left + 1)/10
-    
-    console.log(answer, left)
-    
-    return answer + Math.floor((left+1)/10);
-    
+    answer += Math.floor(left/10);
+  
+    return answer;
 }
